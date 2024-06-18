94a953690324dca322f9177cadd07e8298f2240a
changsu <changsu@xosphere.io>
2024-06-17 17:14:13 -0700

    GCP-170: add output in terraform for topic id and bucket name to be used in other modules

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "drain_topic_name" {
+  value = google_pubsub_topic.k8s_node_draining_topic.id
+}
+
+output "instance_state_bucket_name" {
+  value = google_storage_bucket.instance_state_bucket.name
+}
